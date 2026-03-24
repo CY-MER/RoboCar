9c73b06d101eb794c22167c11cb862cd9086974c
Chris Rhayem <rhayemm.chris@gmail.com>
2026-03-24 19:04:29 +0100

    modif __init__ de source

--- roboCar/Source/__init__.py
+++ roboCar/Source/__init__.py
@@ -1,6 +1,12 @@
 from .Model.simulation import Simulation
 from .Model.robocar import RoboCar
-from .Controler.strategies import GestionStrategies
+from .Controler.strategies import (
+    AvancerXMetres,
+    TournerXDegrees,
+    Reculer,
+    EviterObstacles,
+    GestionStrategies
+)
 from .View_2D.affichage import Affichage
 
 from .Controler.adaptateur_simule import AdaptateurSimule
